22f04014d226ba58ca0d43de5d35ff69a5669900
Jonathas <jonathas09@gmail.com>
2020-04-11 00:38:48 -0300

    Inicial

---
+++ src/homepage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Homepage = () => (
+    <div className='homepage'>
+        <div className='menu'>
+            <div className='menu-item'>
+                <div className='menu-item_content'>
+                    <h1 className='menu-item_content_title'>CARS</h1>
+                    <h2 className='menu-item_content_subtitle'>COMPRE AGORA</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Homepage;
