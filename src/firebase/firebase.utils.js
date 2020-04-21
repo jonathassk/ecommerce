a467a44e46820afa599d3397687afea61da174b2
Jonathas <jonathas09@gmail.com>
2020-04-21 16:14:41 -0300

    configurando conexão com google pelo firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyDRl0jUvUEX0V_dZ8WwZ6nwaWx5tyEALlw",
+  authDomain: "ecommercetest-11ee0.firebaseapp.com",
+  databaseURL: "https://ecommercetest-11ee0.firebaseio.com",
+  projectId: "ecommercetest-11ee0",
+  storageBucket: "ecommercetest-11ee0.appspot.com",
+  messagingSenderId: "565402991332",
+  appId: "1:565402991332:web:26793ecef4faf506248af1",
+  measurementId: "G-C6TRRV370R"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt: 'select_account'});
+export const singInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
